ac9641db2f2e8a6ac6e0436130f45837815edc36
Luís (James) Miguel <lmsandrincardoso@gmail.com>
2026-03-26 20:04:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const addCarrinho = document.querySelectorAll(".adicionar-carrinho");
+const contCarrinho = document.getElementById("carrinho-count");
+
+let qtdCarrinho = 0;
+let produtosCarrinho = [];
+
+function atualizarCarrinho(event) {
+  let produto = event.target.closest(".item-produto");
+  qtdCarrinho++;
+  contCarrinho.textContent = qtdCarrinho;
+
+  let item = {
+    id: produto.dataset.id,
+    name: produto.dataset.name,
+  };
+
+  produtosCarrinho.push(item);
+  console.log(produtosCarrinho);
+}
+
+addCarrinho.forEach(function (button) {
+  button.addEventListener("click", atualizarCarrinho);
+});
